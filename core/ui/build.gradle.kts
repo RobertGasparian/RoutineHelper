ed6d1259f24c97eb153148b422a8fbfb885b3c24
robgasparian <rob.gasparian@gmail.com>
2026-06-17 22:19:24 -0400

    ref: dsm components moved to core ui

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -28,9 +28,13 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.animation.core)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
 }
